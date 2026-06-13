b7f1023755684103c44ac70148a15d4a809c98aa
Xynrin <Xynrin@users.noreply.github.com>
2026-06-13 17:45:06 +0800

    fix: Inno Setup compilation error due to invalid userprofile constant

--- auto_updater.py
+++ auto_updater.py
@@ -8,7 +8,7 @@ import tempfile
 import subprocess
 import tkinter.messagebox as messagebox
 
-CURRENT_VERSION = "v1.5.1"
+CURRENT_VERSION = "v1.5.2"
 
 def check_for_updates(root, silent=True):
     def _run():
